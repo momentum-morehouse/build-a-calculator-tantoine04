c578ff2e911e737a5f32e49effd5a1c3f5aff0a2
Tamlin Antoine <tantoine04@gmail.com>
2020-11-23 18:09:25 +0000

    color and style

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-// let result = document.querySelector(".result");
-//     let btns = document.querySelector(".buttons");
-//     let ops = document.querySelectorAll(".operator");
-    
-    // let buttons = document.querySelectorAll(".buttons")
-    // console.log(buttons);
-    // for(let button of buttons) {
-    //   buttons.addEventListener('click', function() {
-    //   )
-    //   }
-      
-    // }
+// select all the buttons
+let buttons = document.querySelectorAll('button');
+// select the <input type="text" class="display" disabled> element
+let display = document.querySelector('.display');
 
-    function getButton () {
-      let buttons = document.querySelectorAll(".buttons");
-      console.log(buttons)
-      let display = document.querySelector(".result");
-      for (let button of buttons) {
-        console.log(button.textContent);
-        button.addEventListener("click", function () {
-          alert(button.textContent);
-        })
-      }
+// add eventListener to each button
+buttons.forEach(function(button) {
+  button.addEventListener('click', calculate);
+});
+
+// calculate function
+function calculate(event) {
+  // current clicked buttons value
+  let clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === '=') {
+    // check if the display is not empty then only do the calculation
+    if (display.value !== '') {
+      // calculate and show the answer to display
+      display.value = eval(display.value);
     }
-
-    getButton();
-
+  } else if (clickedButtonValue === 'C') {
+    // clear everything on display
+    display.value = '';
+  } else {
+    // otherwise link it to the display
+    display.value += clickedButtonValue;
+  }
+}
